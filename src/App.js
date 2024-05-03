5e1068383efcb2b1415fb962f992970b527bce82
Rodika Aryku <raryku@yahoo.com>
2024-05-03 09:54:10 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+// import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const APP_ID = '4e1c9e3c';
+  const APP_KEY = '7f26ffebc9e182eb8ef5e0a528b49ac3';
+  const APP_URL = "https://api.edamam.com/api/nutrition-details";
+
+ 
+  const fetchData = async (ingr) => {
+    console.log(ingr)
+    const response = await fetch(`${APP_URL}?app_id=${APP_ID}&app_key=${APP_KEY}`, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ingr: ingr })
+    })
+    const data = await response.json();
+    console.log(data)
+  }
+  fetchData()
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test</h1>
     </div>
   );
 }
